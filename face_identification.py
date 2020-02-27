dec3752decf6395fdb193908f75f4b489c081ec7
chaarmarouch <53597121+chaarmarouch@users.noreply.github.com>
2020-02-27 15:53:47 -0600

    Update face_identification.py

--- face_identification.py
+++ face_identification.py
@@ -25,7 +25,6 @@ minH = 0.1*cam.get(4)
 
 while True:
     ret, img =cam.read()
-    img = cv2.flip(img, -1) # Flip vertically
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     
     faces = faceCascade.detectMultiScale( 
